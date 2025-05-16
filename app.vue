7b033212178ca8a9e7898ae1edcf0c0ea9be2c13
ulrichbenedikt <ulrichbenedikt@gmail.com>
2025-05-16 22:22:22 +0200

    moved to kdrive

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
       <img src="/logo.png" alt="Note Hero Logo" class="w-32 h-32 mb-6" />
       <h1 class="text-4xl font-bold mb-4">Note Hero</h1>
       <p class="text-lg text-center max-w-md mb-8">
-        A minimalist note app for focused thinking. Markdown, client linking, shortcuts, and more.
+        A minimalist note app for focused thinking. Markdown, client linking, shortcuts, and more. 
       </p>
       <div class="flex flex-col gap-4 sm:flex-row ">
         <a
@@ -16,24 +16,26 @@
           style="background: linear-gradient(235deg,rgba(253, 29, 29, 1) 0%, rgba(252, 69, 203, 1) 100%);"
           class="bg-white text-white px-6 py-3 rounded-xl opacity-80 hover:opacity-100 transition"
         >
-          Download for macOS
+          Download for macOS<sup>1</sup> 
         </a>
         <a
-          href="https://drive.google.com/file/d/12xinGndcqva5jPnPxk406cwL3oQ15Cxn/view?usp=drive_link"
+          href="https://kdrive.infomaniak.com/app/share/1638485/9acdd6c3-3aa1-4c30-a15b-e08e7db4645a"
           download
           style="background: linear-gradient(203deg,rgba(230, 23, 23, 1) 0%, rgba(245, 108, 208, 1) 100%);"
           class="text-white px-6 py-3 rounded-xl opacity-80 hover:opacity-100  transition"
         >
-          Download for Windows
+          Download for Windows<sup>1</sup> 
         </a>
         
       </div>
+      <p class="text-gray-400 text-xs mt-2">1) The buttons link you to kdrive from <a href="https://www.infomaniak.com/" target="_blank" class="underline">Infomaniak</a>, a secure and ethical, swiss data storage plattform.</p>
       <a
           href="mailto:be@nedikt.be"
           class="mt-8 text-gray-400 px-2 py-1 hover:text-white transition"
         >
           Contact Me
         </a>
+        <p>With ♥️ build in Fürth, Germany.</p>
     </div>
   </main>
 </template>

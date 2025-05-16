b26a049ad29b28b42a788f4e53b108b125f033d4
ulrichbenedikt <ulrichbenedikt@gmail.com>
2025-05-16 22:26:11 +0200

    adding versioning

--- app.vue
+++ app.vue
@@ -16,7 +16,7 @@
           style="background: linear-gradient(235deg,rgba(253, 29, 29, 1) 0%, rgba(252, 69, 203, 1) 100%);"
           class="bg-white text-white px-6 py-3 rounded-xl opacity-80 hover:opacity-100 transition"
         >
-          Download for macOS<sup>1</sup> 
+          Download for macOS<sup>1</sup><br /><span class="text-xs">Version 1.1.0</span>
         </a>
         <a
           href="https://kdrive.infomaniak.com/app/share/1638485/9acdd6c3-3aa1-4c30-a15b-e08e7db4645a"
@@ -24,18 +24,18 @@
           style="background: linear-gradient(203deg,rgba(230, 23, 23, 1) 0%, rgba(245, 108, 208, 1) 100%);"
           class="text-white px-6 py-3 rounded-xl opacity-80 hover:opacity-100  transition"
         >
-          Download for Windows<sup>1</sup> 
+          Download for Windows<sup>1</sup><br /><span class="text-xs">Version 1.1.0</span>
         </a>
         
       </div>
       <p class="text-gray-400 text-xs mt-2">1) The buttons link you to kdrive from <a href="https://www.infomaniak.com/" target="_blank" class="underline">Infomaniak</a>, a secure and ethical, swiss data storage plattform.</p>
       <a
           href="mailto:be@nedikt.be"
-          class="mt-8 text-gray-400 px-2 py-1 hover:text-white transition"
+          class="mt-8 text-white px-2 py-1 opacity-70 hover:opacity-100 transition"
         >
           ✉️ Contact Me
         </a>
-        <p>With ♥️ build in Fürth, Germany.</p>
+        <p class="mt-8">With ♥️ build in Fürth, Germany.</p>
     </div>
   </main>
 </template>
